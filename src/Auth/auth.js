28c570db3b2c079e5b9a6e5be63b80cc3751dfe5
Patrick Gomes <patrick_epcar17@outlook.com>
2022-11-30 22:04:05 -0600

    Update auth.js

--- src/Auth/auth.js
+++ src/Auth/auth.js
@@ -5,10 +5,11 @@ const SECRET = process.env.SECRET;
 
 const validateToken = async (req, res, next) => {
   const header = req.header('Authorization');
-  const decode = jwt.verify(header, SECRET);
-  if (!decode) return res.status(401).json({ message: "Access Denied." });
-  req.userId = decode;
-  next();
+  jwt.verify(header, SECRET, (err, decode) => {
+    if (err) return res.status(401).json({ message: "Access Denied." });
+    req.userId = decode;
+    next();
+  });
 };
 
 module.exports = validateToken;
